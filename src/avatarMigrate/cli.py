45a38f25b663e6997708adc2e5f37abffcf2b43e
Taoufik Janah (TJ) <tj@mode55.co.uk>
2020-10-01 22:16:11 +0100

    Create cli and create parser function to take 2 args source and target backet

--- src/avatarMigrate/cli.py
+++ src/avatarMigrate/cli.py
@@ -2,4 +2,6 @@ from argparse import ArgumentParser
 
 def create_parser():
     parser = ArgumentParser()
+    parser.add_argument('source_bucket', help="S3 source_bucket name")
+    parser.add_argument('target_bucket', help="S3 target_bucket name")
     return parser
