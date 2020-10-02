1e9a821f7d2c96d04e22181d85fbcf7f61267de2
Taoufik Janah (TJ) <tj@mode55.co.uk>
2020-10-02 16:48:38 +0100

    updated the CLI to check for database update flag beofre running dn update

--- src/avatarMigrate/cli.py
+++ src/avatarMigrate/cli.py
@@ -4,10 +4,7 @@ def create_parser():
     parser = ArgumentParser()
     parser.add_argument('source_bucket', help="S3 source_bucket name")
     parser.add_argument('target_bucket', help="S3 target_bucket name")
-    # parser.add_argument('database_url', help="RDS database url")
-    # parser.add_argument('database_username', help="RDS database username")
-    # parser.add_argument('database_password', help="RDS database password")
-    # parser.add_argument('database_name', help="RDS database name")
+    parser.add_argument('update_db', help="Update database records Yes / No")
     return parser
 
 
@@ -19,8 +16,8 @@ def main():
     args = create_parser().parse_args()
     if(args.source_bucket and args.target_bucket):
         copy_objects = s3.migrate_objects(args.source_bucket,args.target_bucket)
-        #if(copy_objects and args.database_url and args.database_username and args.database_password and args.database_name):
-        rds.update_database() 
+        if(copy_objects and args.update_db=='Yes'):
+            rds.update_database() 
 
 if __name__ == "__main__":
     main()
